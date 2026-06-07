397aac810edfaa586b7afae39fa1e71cb768af9c
DotSnuk <hampusfarner@outlook.com>
2026-06-07 11:14:19 +0200

    Basic tcplistener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::{
+    fs,
+    io::{BufReader, prelude::*},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_req: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    let status_line = "HTTP/1.1 200 OK";
+    let content = fs::read_to_string("template.html").unwrap();
+    let length = content.len();
+    let response = format!("{status_line}\r\nContent-length: {length}\r\n\r\n{content}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
